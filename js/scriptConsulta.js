c0ae874bed4264df7422859774d6062f575a23c0
Isaque <isaque.severino@unifesspa.edu.br>
2025-07-07 18:33:42 -0300

    geralzona

--- js/scriptConsulta.js
+++ js/scriptConsulta.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  const usuarioLogadoId = sessionStorage.getItem('usuarioLogadoId');
+  const usuarioLogado = JSON.parse(localStorage.getItem('usuarios')).find(usuario => usuario.id === usuarioLogadoId);
+
+  const nomeUsuario = usuarioLogado ? usuarioLogado.nome : 'Visitante';
+
+  const nomeUsuarioElement = document.getElementById('perfil-span');
+  nomeUsuarioElement.textContent = nomeUsuario;
+
   const estadoSelect = document.getElementById("estado");
   const cidadeSelect = document.getElementById("cidade");
   const hospitalSelect = document.getElementById("hospital");
